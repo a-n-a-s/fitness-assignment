e9c5be959d2f2e79fbe98ef5e5ce2e692cc5805d
a-n-a-s <mirzaanas815@gmail.com>
2026-05-31 19:57:54 +0530

    about page is done

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,13 +4,13 @@ const Card = () => {
   return (
     <div className="my-20">
       <div className="my-10 w-5/6 bg-lime-400 text-black h-40 mx-auto flex items-center justify-start p-4  rounded-lg">
-        <p className="text-6xl font-bold text-center">Personal Training</p>
+        <p className="lg:text-6xl font-bold text-center text-2xl">Personal Training</p>
       </div>
-      <div className="my-10 w-5/6 bg-black text-lime-400 h-40 mx-auto flex items-center justify-end p-4 rounded-lg">
-        <p className="text-6xl font-bold text-center">Group Fitness Classes</p>
+      <div className="my-10 w-5/6 bg-black text-lime-400 h-40 mx-auto flex items-center md:justify-end justify-start p-4 rounded-lg">
+        <p className="lg:text-6xl font-bold text-center text-2xl">Group Fitness Classes</p>
       </div>
       <div className="my-10 w-5/6 bg-lime-400 text-black h-40 mx-auto flex items-center justify-start p-4  rounded-lg">
-        <p className="text-6xl font-bold text-center">Functional Training</p>
+        <p className="lg:text-6xl font-bold text-center text-2xl">Functional Training</p>
       </div>
     </div>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-    <div className='text-black bg-lime-400 h-96 mt-20 text-8xl flex flex-col items-center justify-center gap-10'>
+    <div className='text-black bg-lime-400 h-96 mt-20 lg:text-8xl md:text-5xl text-3xl flex flex-col items-center justify-center gap-10'>
        <p>YOUR BODY </p>
        <p>IS YOUR TEMPLE</p>
     </div>
-    <p className='text-white text-lg  text-center my-8'>Join the club today!</p>
+    <p className='text-white md:text-lg text-md  text-center my-8'>Join the club today!</p>
     </>
   )
 }
